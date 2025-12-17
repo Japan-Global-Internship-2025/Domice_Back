07281468fcc70f384c50317e5e0f7b1918f20f1d
minjae1025 <kjt081025@gmail.com>
2025-12-17 16:31:25 +0900

    쿠키 보안

--- server.js
+++ server.js
@@ -1609,10 +1609,10 @@ app.post("/api/auth/login", async (req, res) => {
     res.cookie('access_token', token, {
       maxAge: 1000 * 60 * 60 * 24 * 30,
       httpOnly: true,
-      // secure: req.secure,
-      secure: true,
-      // sameSite: req.secure ? 'None' : 'lax',
-      sameSite: 'none'
+      secure: req.secure,
+      // secure: true,
+      sameSite: req.secure ? 'None' : 'lax',
+      // sameSite: 'none'
     });
 
     return sendOk(res, userData);
@@ -1631,10 +1631,10 @@ app.post("/api/auth/login", async (req, res) => {
 app.get("/api/auth/logout", async (req, res) => {
   res.clearCookie('access_token', {
     httpOnly: true,
-    // secure: req.secure,
-    secure: true,
-    // sameSite: req.secure ? 'None' : 'lax',
-    sameSite: 'none'
+    secure: req.secure,
+    // secure: true,
+    sameSite: req.secure ? 'None' : 'lax',
+    // sameSite: 'none'
   });
 
   return sendOk(res, { success: "true" });
